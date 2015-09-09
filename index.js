0db6d14f2d14e2c76114b350671cd731c052d2b1
钱啸 <xqian@njgufun.com>
2015-09-09 18:32:49 +0800

    - modules: mongoose, morgan, uuid, base64-min - wechat agent test passed.

--- index.js
+++ index.js
@@ -8,34 +8,37 @@
 
 module.exports = {
 
-	database: {
-		Database: require('./lib/database/database'),
-		Model: require('./lib/database/model')
-},
+	database:  {
+		Database: require( './lib/database/database' ),
+		Model:    require( './lib/database/model' )
+	},
 	viewModel: {
-		Page: require('./lib/viewModel/page'),
-		Project: require('./lib/viewModel/project'),
-		Filter: require('./lib/viewModel/filter'),
-		HbsHelper: require('./lib/viewModel/hbsHelper')
+		Page:      require( './lib/viewModel/page' ),
+		Project:   require( './lib/viewModel/project' ),
+		Filter:    require( './lib/viewModel/filter' ),
+		HbsHelper: require( './lib/viewModel/hbsHelper' )
 	},
-	utils: {
-		common: require('./lib/utils/index'),
-		Interface: require('./lib/utils/interface'),
-		underscore: require('underscore'),
-		EventProxy: require('eventproxy'),
-		moment: require('moment'),
-		urllib: require('urllib')
+	utils:     {
+		common:     require( './lib/utils/index' ),
+		Interface:  require( './lib/utils/interface' ),
+		underscore: require( 'underscore' ),
+		EventProxy: require( 'eventproxy' ),
+		moment:     require( 'moment' ),
+		urllib:     require( 'urllib' ),
+		base64:     require( 'base64-min' )
 	},
-	crypto: {
-		MixCrypto: require('./lib/crypto/mixCrypto')
+	crypto:    {
+		MixCrypto: require( './lib/crypto/mixCrypto' )
 	},
-	wechat: {
-		api: require('wechat-api'),
-		pay: require('wechat-pay'),
-		auth: require('wechat-oauth'),
-		middleware: require('wechat'),
-		proxy: require('./lib/app/wechat/proxy')
+	wechat:    {
+		api:        require( 'wechat-api' ),
+		pay:        require( 'wechat-pay' ),
+		auth:       require( 'wechat-oauth' ),
+		middleware: require( 'wechat' ),
+		proxy:      require( './lib/app/wechat/proxy' ),
+		Agent:      require( './lib/app/wechat/agent' ),
+		Message:    require( './lib/app/wechat/message' )
 	},
-	logger: require('./lib/logger')
+	logger:    require( './lib/logger' )
 
 };
